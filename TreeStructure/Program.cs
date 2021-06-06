fac4107cb1ad24163ea7c1a9f9b798c8e997b90b
ChungLee Ochieze <chunglee.ochieze@yahoo.co.uk>
2021-06-06 16:41:26 +1000

    static program class

--- TreeStructure/Program.cs
+++ TreeStructure/Program.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 
 namespace TreeStructure
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
